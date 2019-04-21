0ab74c784d536ca2a77b5127d8082f1bfe7f0fd7
Sergio Belda <serbelga@inf.upv.es>
2019-04-21 19:02:52 +0200

    New icons + modification of fragment detail UI

--- android_app/app/src/main/java/com/example/sergiobelda/iot_cloud_weather/livedata/WeatherStateLiveData.kt
+++ android_app/app/src/main/java/com/example/sergiobelda/iot_cloud_weather/livedata/WeatherStateLiveData.kt
@@ -1,7 +1,9 @@
 package com.example.sergiobelda.iot_cloud_weather.livedata
 
+import android.text.format.DateFormat
 import androidx.lifecycle.LiveData
 import com.example.sergiobelda.iot_cloud_weather.model.WeatherState
+import com.google.firebase.Timestamp
 import com.google.firebase.firestore.*
 
 class WeatherStateLiveData(private val documentReference: DocumentReference) : LiveData<WeatherState>(), EventListener<DocumentSnapshot> {
@@ -18,11 +20,14 @@ class WeatherStateLiveData(private val documentReference: DocumentReference) : L
     override fun onEvent(snapshot: DocumentSnapshot?, exception: FirebaseFirestoreException?) {
         if (snapshot != null && snapshot.exists()) {
             val state = snapshot.get("state") as HashMap<String, Number>
+            val timestamp = snapshot.get("timestamp") as Timestamp
+            val dateFormat = DateFormat.format("dd-MM-yyyy HH:mm:ss", timestamp.toDate()).toString()
+            val online = snapshot.get("online") as Boolean
             val weatherState = WeatherState(
                 temperature = state["temperature"]!!.toDouble(),
                 humidity = state["humidity"]!!.toDouble(),
-                lastConnection = "21/12/2018",
-                online = true
+                lastConnection = dateFormat,
+                online = online
             )
             value = weatherState
         } else if (exception != null) {
--- android_app/app/src/main/java/com/example/sergiobelda/iot_cloud_weather/ui/DetailFragment.kt
+++ android_app/app/src/main/java/com/example/sergiobelda/iot_cloud_weather/ui/DetailFragment.kt
@@ -1,15 +1,20 @@
 package com.example.sergiobelda.iot_cloud_weather.ui
 
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+
 import com.example.sergiobelda.iot_cloud_weather.R
-import com.example.sergiobelda.iot_cloud_weather.model.WeatherState
 import com.example.sergiobelda.iot_cloud_weather.viewmodel.WeatherStateViewModel
 import kotlinx.android.synthetic.main.fragment_detail.*
 
@@ -33,12 +38,21 @@ class DetailFragment : Fragment() {
         val id = "esp8266_4CB5CD"
         deviceId.text = "Device ID: $id"
         val viewModel = ViewModelProviders.of(this).get(WeatherStateViewModel::class.java)
+
         val liveData = viewModel.getWeatherStateLiveData("esp8266_4CB5CD")
         liveData.observe(this, Observer { weatherState ->
             if (weatherState != null) {
+                val online = if (weatherState.online) "Connected" else "Disconnected"
+                val color = if (weatherState.online) R.color.teal else R.color.red
+                val spannable = SpannableString("${online} - Last update: ${weatherState.lastConnection}")
+                spannable.setSpan(
+                    ForegroundColorSpan(ContextCompat.getColor(context!!, color)),
+                    0, online.length,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
                 temp.text = weatherState.getTemperatureString()
                 humidity.text = weatherState.getHumidityString()
-                connectionDetails.text = "Status: ${weatherState.online} - Last connection: ${weatherState.lastConnection}"
+                connectionDetails.text = spannable
             }
         })
     }
