fc9023dfba1b50b30bfa48965f90ebc0eda911ae
Sergio Belda <serbelga@inf.upv.es>
2019-04-25 12:53:38 +0200

    New function state - gets the online state of device

--- functions/index.js
+++ functions/index.js
@@ -57,3 +57,30 @@ exports.functionArray = functions.pubsub.topic('iot-topic').onPublish(async (mes
     console.error(`${deviceId} not yet registered to a user`, error);
   }
 });
+
+
+exports.state = functions.pubsub.topic('online-state').onPublish(async (message) => {
+  const logEntry = JSON.parse(Buffer.from(message.data, 'base64').toString());
+  const deviceId = logEntry.labels.device_id;
+
+  let online;
+  switch (logEntry.jsonPayload.eventType) {
+    case 'CONNECT':
+      online = true;
+      break;
+    case 'DISCONNECT':
+      online = false;
+      break;
+    default:
+      throw new Error(`Invalid event type received from IoT Core: ${logEntry.jsonPayload.eventType}`);
+  }
+
+  // Write the online state into firestore
+  const deviceRef = firestore.doc(`device-configs/${deviceId}`);
+  try {
+    await deviceRef.update({ 'online': online });
+    console.log(`Connectivity updated for ${deviceId}`);
+  } catch (error) {
+    console.error(`${deviceId} not yet registered to a user`, error);
+  }
+});
